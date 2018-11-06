3d5a00711d14271fe14c371ae0d3caff63193c3d
dowler <dowler@umich.edu>
2018-11-05 19:05:38 -0500

    one more time

--- rpn.py
+++ rpn.py
@@ -1,59 +1,20 @@
 #!/usr/bin/env python3
 
-import operator, math
+import operator
 
-def percentage(a, b):
-    return (a / float(b)) * 100
 
-def exponent(a, b):
-    return operator.pow(a, b)
-
-def intdiv(a, b):
-    return operator.floordiv(a, b)
-
-def andfunc(a, b):
-    return operator.and_(a, b)
-
-def orfunc(a, b):
-    return operator.or_(a, b)
-
-def notfunc(a, b):
-    return operator.xor(a, b)
-
- 
 operators = {
     '+': operator.add,
     '-': operator.sub,
     '*': operator.mul,
     '/': operator.truediv,
-    '%': percentage,
-    '^': exponent,
-    '//': intdiv,
-    '&': andfunc,
-    '|': orfunc,
-    '!': notfunc,
-
 }
 
 def calculate(myarg):
     stack = list()
     for token in myarg.split():
         try:
-            if token == 'pi':
-                token = math.pi
-            elif token == 'e':
-                token = math.e
-            elif token == 'sin':
-               result = math.sin(stack.pop())
-                stack.append(result)
-                break
-            elif token == 'cos':
-                result = math.cos(stack.pop())
-                stack.append(result)
-                break
-            else:
-                token = int(token)
-    
+            token = int(token)
             stack.append(token)
         except ValueError:
             function = operators[token]
